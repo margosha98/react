3b3e6cffb2de04d02af8556cd7c65932fefe888f
margosha98 <nesterenko.margaritaa@gmail.com>
2022-11-08 03:33:45 +0200

    Lesson 12. Components

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import react from "react"
+
+const Header = () => {
+    return(
+        <header className='header'> 
+            <img src='https://static.wixstatic.com/media/080735_fb4667a2fa4645c7992752c9a530b532~mv2.png/v1/fill/w_800,h_800,al_c/080735_fb4667a2fa4645c7992752c9a530b532~mv2.png'/>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,26 @@
+import react from "react"
+
+const Profile = () => {
+    return(
+        <div className='content'>
+            <div>
+            <img src ='https://oir.mobi/uploads/posts/2019-12/1576101448_1-2.jpg'/>
+            </div>
+            <div>
+            Avatar + discription
+            </div>
+            <div>
+            My posts
+            <div>
+                New post
+            </div>
+            </div>
+            <div>
+                <div> Post 1</div>
+                <div> Post 2</div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
